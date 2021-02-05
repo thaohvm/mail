9536ef0a49e1ea8c104e8699f0f98b3e0fa57a48
thaohvm <43254779+thaohvm@users.noreply.github.com>
2021-02-04 22:40:26 -0800

    Load email content

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -24,56 +24,56 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  const email_view = document.querySelector('#emails-view');
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-  
+
   const mailTable = document.createElement('table');
   mailTable.classList.add('table');
   mailTable.classList.add('table-sm');
   mailTable.classList.add('table-bordered');
 
-  email_view.appendChild(mailTable);
-  
+  const emails_view = document.querySelector('#emails-view');
+  emails_view.appendChild(mailTable);
+
   fetch(`/emails/${mailbox}`)
     .then(response => response.json()
-    .then(emails => {
-      emails.forEach(function(email) {
-        const mail = document.createElement('tr');
-        mail.addEventListener('click', function(){
-          load_email(email["id"]);
-        });
-        mailTable.appendChild(mail);
-        
-        if (mailbox === "sent") {
-          const recipients = document.createElement('td');
-          recipients.innerHTML = `${email["recipients"].join(", ")}`;
-          mail.appendChild(recipients);
+      .then(emails => {
+        emails.forEach(function (email) {
+          const mail = document.createElement('tr');
+          mail.addEventListener('click', function () {
+            load_email(email["id"]);
+          });
+          mailTable.appendChild(mail);
 
-        } else if (mailbox === "inbox") {
-          const sender = document.createElement('td');
-          sender.innerHTML = `${email["sender"]}`;
-          mail.appendChild(sender);
-        }
+          if (mailbox === "sent") {
+            const recipients = document.createElement('td');
+            recipients.innerHTML = `${email["recipients"].join(", ")}`;
+            mail.appendChild(recipients);
 
-        const subject = document.createElement('td');
-        subject.innerHTML = `${email["subject"]}`;
+          } else if (mailbox === "inbox") {
+            const sender = document.createElement('td');
+            sender.innerHTML = `${email["sender"]}`;
+            mail.appendChild(sender);
+          }
 
-        const timestamp = document.createElement('td');
-        timestamp.innerHTML = `${email["timestamp"]}`;
-        timestamp.style.color = "gray";
-        timestamp.style.textAlign = "right";
+          const subject = document.createElement('td');
+          subject.innerHTML = `${email["subject"]}`;
 
-        mail.appendChild(subject);
-        mail.appendChild(timestamp);
+          const timestamp = document.createElement('td');
+          timestamp.innerHTML = `${email["timestamp"]}`;
+          timestamp.style.color = "gray";
+          timestamp.style.textAlign = "right";
 
-      })
-    }))
+          mail.appendChild(subject);
+          mail.appendChild(timestamp);
+
+        })
+      }))
     .catch((error) => console.error(error));
-  };
+};
 
 function send_email(event) {
   event.preventDefault();
@@ -99,5 +99,36 @@ function send_email(event) {
 }
 
 function load_email(id) {
-  console.log(id);
+  document.querySelector('#emails-view').style.display = "none";
+
+  const email_view = document.querySelector('#email-view');
+  email_view.style.display = "block";
+
+  fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(email => {
+      email_view.innerHTML = `
+        <p><strong>From: </strong> ${email["sender"]}</p>
+        <p><strong>To: </strong> ${email["recipients"].join(", ")}</p>
+        <p><strong>Subject: </strong> ${email["subject"]}</p>
+        <p><strong>Timestamp: </strong> ${email["timestamp"]}</p>
+        <button class="btn btn-sm btn-outline-primary" id="email-view-reply">Reply</button>
+        <hr>
+        <p>${email["body"]}</p>
+      `
+      document.querySelector('#email-view-reply').addEventListener('click', () => reply_email());
+
+      // Mark email as read
+      fetch(`/emails/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          read: true
+        })
+      })
+    })
+    .catch((error) => console.log(error));
+}
+
+function reply_email() {
+  console.log("Reply clicked!")
 }
